1c16df6773025817ae1901e60cbe5b31159404d1
cesar <cesar@herondata.io>
2021-11-15 10:51:07 +0000

    change favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/icon.png',
   organizationName: 'heron-data', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
   presets: [
