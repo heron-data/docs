22e83a9ffbdbab07e687d1dee4e8838d931cf182
Dom Kwok <dominictskwok@gmail.com>
2023-04-11 12:53:43 +0100

    try fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,126 +1,130 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+console.log(process.env.SPEC_URL);
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Heron Data Documentation',
-  tagline: 'Transaction Enrichment and Categorization API',
-  url: 'https://docs.herondata.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/icon.png',
-  organizationName: 'heron-data',
-  projectName: 'docs',
+  title: "Heron Data Documentation",
+  tagline: "Transaction Enrichment and Categorization API",
+  url: "https://docs.herondata.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/icon.png",
+  organizationName: "heron-data",
+  projectName: "docs",
   presets: [
     [
-      'redocusaurus',
+      "redocusaurus",
       {
-        specs: [{
-          routePath: '/api',
-          specUrl: process.env.SPEC_URL,
-          layout: {
-            title: 'API Reference'
-          }
-        }]
-      }
+        specs: [
+          {
+            route: "/api",
+            spec: process.env.SPEC_URL,
+            layout: {
+              title: "API Reference",
+            },
+          },
+        ],
+      },
     ],
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/heron-data/docs/tree/main',
-          routeBasePath: '/'
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/heron-data/docs/tree/main",
+          routeBasePath: "/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css')
-        }
-      })
-    ]
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
   ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Heron Data',
+        title: "Heron Data",
         logo: {
-          alt: 'Heron Data Logo',
-          src: 'img/icon.png'
+          alt: "Heron Data Logo",
+          src: "img/icon.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial'
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
           },
           {
-            to: '/api',
-            label: 'API Reference',
-            position: 'left'
+            to: "/api",
+            label: "API Reference",
+            position: "left",
           },
           {
-            to: 'https://status.herondata.io',
-            label: 'Status',
-            position: 'right'
-          }
-        ]
+            to: "https://status.herondata.io",
+            label: "Status",
+            position: "right",
+          },
+        ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/'
+                label: "Tutorial",
+                to: "/",
               },
               {
-                label: 'API Reference',
-                to: '/api'
-              }
-            ]
+                label: "API Reference",
+                to: "/api",
+              },
+            ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Blog',
-                to: 'https://www.herondata.io/blog'
+                label: "Blog",
+                to: "https://www.herondata.io/blog",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/herondata'
-              }
-            ]
+                label: "Twitter",
+                href: "https://twitter.com/herondata",
+              },
+            ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Website',
-                href: 'https://www.herondata.io'
+                label: "Website",
+                href: "https://www.herondata.io",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/heron-data'
-              }
-            ]
-          }
+                label: "GitHub",
+                href: "https://github.com/heron-data",
+              },
+            ],
+          },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Open Credit Technologies`
+        copyright: `Copyright © ${new Date().getFullYear()} Open Credit Technologies`,
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
-      }
-    })
-}
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
 
-module.exports = config
+module.exports = config;
